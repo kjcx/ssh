7ebccf0532ee1088ae4523c9647bd25e30716a4b
kjcx <1339796113@qq.com>
2020-06-04 11:36:18 +0800

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -1,16 +1,13 @@
 (
 cat << EOF
-Host $1
-HostName $2
+
+Host $2
+HostName $1
 User root
 PreferredAuthentications publickey
  IdentityFile ~/.ssh/id_rsa
 EOF
 ) >> config
-cat << EOF
-ClientAliveInterval 30
-ClientAliveCountMax 86400
-EOF
-) >> /etc/ssh/sshd_config
 
-scp -p ~/.ssh/id_rsa.pub root@$2:/root/.ssh/authorized_keys
+scp -P $3 -p ~/.ssh/id_rsa.pub root@$1:/root/.ssh/authorized_keys
+ssh $2
